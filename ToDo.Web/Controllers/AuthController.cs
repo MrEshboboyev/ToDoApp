47c361cf1f20c438844559286cdbc623246bd080
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-09 23:09:03 +0500

    Register() action [HttpPost] writed

--- ToDo.Web/Controllers/AuthController.cs
+++ ToDo.Web/Controllers/AuthController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ToDo.Web.Models;
+using ToDo.Web.Service.IService;
 using ToDo.Web.Utility;
 
 namespace ToDo.Web.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService; 
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -31,6 +39,37 @@ namespace ToDo.Web.Controllers
             ViewBag.RoleList = roleList;
 
             return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if (result != null && result.IsSuccess)
+            {
+                if(string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if(assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successfully!";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text = SD.RoleAdmin, Value = SD.RoleAdmin},
+                new SelectListItem(){Text = SD.RoleCustomer, Value = SD.RoleCustomer}
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View(obj);
         }
 
         [HttpGet]
