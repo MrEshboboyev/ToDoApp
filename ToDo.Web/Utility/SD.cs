92590951f620e2b45cddf8b2481bcf9bf8d39499
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-09 21:24:38 +0500

    Create SD.cs

---
+++ ToDo.Web/Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDo.Web.Utility
+{
+    public class SD
+    {
+        public static string AuthAPIBase { get; set; }
+        public enum ApiType
+        {
+            GET,
+            POST, 
+            PUT, 
+            DELETE
+        }
+    }
+}
