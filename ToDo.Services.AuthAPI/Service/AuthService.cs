16a45ddf9ebd959549d3ce9ed3c9afd7e4ea070a
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-09 18:29:58 +0500

    Inject DI this services : AppDbContext, UserManager, RoleManager

--- ToDo.Services.AuthAPI/Service/AuthService.cs
+++ ToDo.Services.AuthAPI/Service/AuthService.cs
@@ -1,10 +1,27 @@
-﻿using ToDo.Services.AuthAPI.Models.Dto;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using ToDo.Services.AuthAPI.Data;
+using ToDo.Services.AuthAPI.Models;
+using ToDo.Services.AuthAPI.Models.Dto;
 using ToDo.Services.AuthAPI.Service.IService;
 
 namespace ToDo.Services.AuthAPI.Service
 {
     public class AuthService : IAuthService
     {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> __roleManager;
+
+        public AuthService(AppDbContext db,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            __roleManager = roleManager;
+        }
+
         public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             throw new NotImplementedException();
