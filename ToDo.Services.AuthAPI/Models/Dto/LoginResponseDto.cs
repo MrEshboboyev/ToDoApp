67f57ed67636a8cefacbf4533731684f09f00641
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-09 18:05:00 +0500

    Create LoginResponseDto.cs

---
+++ ToDo.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDo.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
