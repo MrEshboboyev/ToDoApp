8e5fb58353d001d111c544b7287538692dcaccf3
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-09 22:37:20 +0500

    Create AuthController.cs; IsAuthneticated condition in _Layout.cshtml;

---
+++ ToDo.Web/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.Web.Models;
+
+namespace ToDo.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
