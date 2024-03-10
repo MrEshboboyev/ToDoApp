01d12290835954db809d0c53c46d1176ca807cec
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-10 12:09:46 +0500

    Create ITokenProvider.cs and TokenProvider.cs;

---
+++ ToDo.Web/Service/TokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using ToDo.Web.Service.IService;
+
+namespace ToDo.Web.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        public void ClearToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
