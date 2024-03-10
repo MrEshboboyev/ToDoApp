2e6421ca084d4f1dfb0a0b5b9099be8029dd859f
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-10 17:57:45 +0500

    Create TaskAPIController.cs and Inject services : ResponseDto,  IMapper, AppDbContext

---
+++ ToDo.Services.TaskAPI/Controllers/TaskAPIController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDo.Services.TaskAPI.Data;
+using ToDo.Services.TaskAPI.Models.Dto;
+
+namespace ToDo.Services.TaskAPI.Controllers
+{
+    [Route("api/task")]
+    [ApiController]
+    public class TaskAPIController : ControllerBase
+    {
+        private ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+
+        public TaskAPIController(AppDbContext db,
+            IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+    }
+}
