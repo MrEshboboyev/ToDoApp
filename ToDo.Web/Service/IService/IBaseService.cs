c6e04f2fad633c6475222ebad008dc1695644408
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-10 16:51:43 +0500

    withBeare argument in SendAsync() in BaseService.cs

--- ToDo.Web/Service/IService/IBaseService.cs
+++ ToDo.Web/Service/IService/IBaseService.cs
@@ -4,6 +4,6 @@ namespace ToDo.Web.Service.IService
 {
     public interface IBaseService
     {
-        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+        Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true);
     }
 }
