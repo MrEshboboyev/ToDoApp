0d4cdd50a13e1a8eb8b0734c2013b90b16c13899
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-10 13:01:27 +0500

    IEnumerable<string> roles added claims

--- ToDo.Services.AuthAPI/Service/AuthService.cs
+++ ToDo.Services.AuthAPI/Service/AuthService.cs
@@ -55,7 +55,8 @@ namespace ToDo.Services.AuthAPI.Service
             }
 
             // if user was found, Generate JWT token
-            var token = _jwtTokenGenerator.GenerateToken(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var token = _jwtTokenGenerator.GenerateToken(user, roles);
 
             UserDto userDto = new()
             {
